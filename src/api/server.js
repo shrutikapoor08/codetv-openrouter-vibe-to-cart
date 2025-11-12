4d7922d4ccaf2890ddd397eef1c706b5ebf8256f
shrutikapoor08 <shruti.kapoor08@gmail.com>
2025-11-11 22:33:54 -0800

    try: kim kardashian meets indian queen

--- src/api/server.js
+++ src/api/server.js
@@ -21,6 +21,12 @@ app.use((req, res, next) => {
   res.set("Access-Control-Allow-Origin", "*");
   res.set("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
   res.set("Access-Control-Allow-Headers", "Content-Type");
+
+  // Handle preflight OPTIONS requests
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
   next();
 });
 
